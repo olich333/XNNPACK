b68d3f7c884bfb461628f97a8ae5b949880132ea
fbarchard <fbarchard@google.com>
2020-07-08 12:48:36 -0700

    Cortex A7 microkernel based on LD64 with PLD added.  3.2% faster in end to end mobilenet v2 PLD instructions moved to end of loop to improve VMLA performance. pld_ld64 microkernel removed.
    Was
    MobileNetV2_F32/XNNPACK/T:1/real_time     511808 us       509497 us           14 FLOPS=1.17534G/s FPS=1.95386/s Freq=1.1904G
    
    Now
    MobileNetV2_F32/XNNPACK/T:1/real_time     496032 us       496007 us           14 FLOPS=1.21273G/s FPS=2.016/s Freq=1.1904G
    
    PiperOrigin-RevId: 320245578

--- scripts/generate-f32-gemm.sh
+++ scripts/generate-f32-gemm.sh
@@ -114,7 +114,7 @@ tools/xngen src/f32-gemm/4x8-aarch32-neon-cortex-a75.S.in  -D INC=0 -D PREFETCH=
 tools/xngen src/f32-gemm/4x8-aarch32-neon-cortex-a75.S.in  -D INC=0 -D PREFETCH=1 -o src/f32-gemm/gen/4x8-minmax-aarch32-neon-pld-cortex-a75.S
 
 tools/xngen src/f32-gemm/4x8-minmax-aarch32-neon-ld64.S.in -D INC=0 -D PREFETCH=0 -o src/f32-gemm/gen/4x8-minmax-aarch32-neon-ld64.S
-tools/xngen src/f32-gemm/4x8-minmax-aarch32-neon-ld64.S.in -D INC=0 -D PREFETCH=1 -o src/f32-gemm/gen/4x8-minmax-aarch32-neon-pld-ld64.S
+tools/xngen src/f32-gemm/4x8-minmax-aarch32-neon-ld64.S.in -D INC=0 -D PREFETCH=1 -o src/f32-gemm/gen/4x8-minmax-aarch32-neon-cortex-a7.S
 
 ################################### ARM NEON ##################################
 ### LD64 micro-kernels
